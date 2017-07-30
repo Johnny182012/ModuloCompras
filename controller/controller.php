f187a3355ac80545961f18f814f9fb2ae22f1d61
Saul Cisneros <cisneross.saul@gmail.com>
2017-07-30 12:04:45 -0500

    Clase Usuario Metodos CRUD Casos controller

--- controller/controller.php
+++ controller/controller.php
@@ -1,8 +1,109 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+///////////////////////////////////////////////////////////////////////
+//Componente controller que verifica la opcion seleccionada          //
+//por el usuario, ejecuta el modelo y enruta la navegacion de paginas//
+///////////////////////////////////////////////////////////////////////
+require_once '../model/CrudModel.php';
 
+session_start();
+//instanciamos los objetos del pedido:
+$crudModel = new CrudModel();
+
+//recibimos la opcion desde la vista:
+$opcion = $_REQUEST['opcion'];
+//limpiamos cualquier mensaje previo:
+unset($_SESSION['mensaje']);
+unset($_SESSION['mensaje1']);
+unset($_SESSION['mensaje2']);
+
+switch ($opcion) {
+
+    // obtiene los datos de los usuarios de la base de datos
+    case "listar_usuarios":
+        //obtenemos la lista de usuarios:
+        $listaUsuarios = $crudModel->getUsuarios();
+        //y los guardamos en sesion:
+        $_SESSION['listaUsuarios'] = serialize($listaUsuarios);
+        //redireccionamos a una nueva pagina para visualizar:
+        header('Location: ../view/usuarios.php');
+        break;
+
+    case "crear_usuario":
+        //obtenemos los parametros del formulario usuario
+        $idusuario = $_REQUEST['idusuario'];
+        $tipoidusuario = $_REQUEST['tipoidusuario'];
+        $rolusuario = $_REQUEST['rolusuario'];
+        $nombreusuario = $_REQUEST['nombreusuario'];
+        $fecnacusuario = $_REQUEST['fecnacusuario'];
+        $ciunacusuario = $_REQUEST['ciunacusuario'];
+        $direccionusuario = $_REQUEST['direccionusuario'];
+        $telefonousuario = $_REQUEST['telefonousuario'];
+        $emailusuario = $_REQUEST['emailusuario'];
+        $estadousuario = $_REQUEST['estadousuario'];
+        //creamos el nuevo listado
+        $crudModel->insertarUsuario($idusuario, $tipoidusuario, $rolusuario, $nombreusuario, $fecnacusuario, $ciunacusuario, $direccionusuario, $telefonousuario, $emailusuario, $estadousuario);
+        //actualizamos el lsitado
+        $listaUsuarios = $crudModel->getUsuarios();
+        //y los guardamos en session
+        $_SESSION['listaUsuarios'] = serialize($listaUsuarios);
+        //redireccionamos a una nueva pagina para visualizar
+        header('Location: ../view/usuarios.php');
+        break;
+    
+    //elimina un usuario especifico
+    case "eliminar_usuario":
+        //obtenemos el codigo del usuario a eliminar:
+        $idusuario = $_REQUEST['idusuario'];
+        //eliminamos del formulario:
+        try {
+            $crudModel->eliminarUsuario($idusuario);
+        } catch (Exception $e) {
+            //colocamos el mensaje de la excepcion en sesion:
+            $_SESSION['mensaje2'] = $e->getMessage();
+        }
+        //actualizamos la lista de usuario para grabar en sesion:
+        $listaUsuarios = $crudModel->getUsuarios();
+        $_SESSION['listaUsuarios'] = serialize($listaUsuarios);
+        //redireccionamos a una nueva pagina para visualizar:
+        header('Location: ../view/usuarios.php');
+        break;
+    //edita los datos de un usuario especifico
+    case "editar_usuario":
+        //obtenemos los parametros del formulario usuario:
+        $idusuario = $_REQUEST['idusuario'];
+        //Buscamos los datos
+        $listaUsuarios = $crudModel->getUsuario($idusuario);
+        //guardamos en sesion para edicion posterior:
+        $_SESSION['listaUsuarios'] = serialize($listaUsuarios);
+        //redirigimos la navegación al formulario de edicion usuario:
+        header('Location: ../view/editarUsuario.php');
+        break;
+    //actualiza los datos de un usuario especifico
+    case "actualizar_usuario":
+        //obtenemos los parametros del formulario usuario:
+        $idusuario = $_REQUEST['idusuario'];
+        $tipoidusuario = $_REQUEST['tipoidusuario'];
+        $rolusuario = $_REQUEST['rolusuario'];
+        $nombreusuario = $_REQUEST['nombreusuario'];
+        $fecnacusuario = $_REQUEST['fecnacusuario'];
+        $ciunacusuario = $_REQUEST['ciunacusuario'];
+        $direccionusuario = $_REQUEST['direccionusuario'];
+        $telefonousuario = $_REQUEST['telefonousuario'];
+        $emailusuario = $_REQUEST['emailusuario'];
+        $estadousuario = $_REQUEST['estadousuario'];
+
+        //actualizamos los datos del usuario:
+        $crudModel->actualizarUsuario($idusuario, $tipoidusuario, $rolusuario, $nombreusuario, $fecnacusuario, $ciunacusuario, $direccionusuario, $telefonousuario, $emailusuario, $estadousuario);
+        //actualizamos lista de usuario:
+        $listaUsuarios = $crudModel->getUsuarios();
+        $_SESSION['listaUsuarios'] = serialize($listaUsuarios);
+        //redireccionamos a una nueva pagina para visualizar el cambio:
+        header('Location: ../view/usuarios.php');
+        break;
+    
+    default:
+        //si no existe la opcion recibida por el controlador, siempre
+        //redirigimos la navegacion a la pagina index:
+        header('Location: ../view/indexLogin.php');
+}
