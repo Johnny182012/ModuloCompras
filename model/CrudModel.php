ba8da73810a1299adc562efb7173f9354e17a03a
Saul Cisneros <cisneross.saul@gmail.com>
2017-07-30 12:08:02 -0500

    Métodos CrudModel

--- model/CrudModel.php
+++ model/CrudModel.php
@@ -116,4 +116,102 @@ class CrudModel {
         Database::disconnect();
     }
 
+    //////////////////////////
+    //CRUD: USUARIOS       //
+    /////////////////////////
+
+    /**
+     * Retorna la lista de usuarios de la bdd.
+     * @return array
+     */
+    public function getUsuarios() {
+        //obtenemos la informacion de la bdd:
+        $pdo = Database::connect();
+        $sql = "select * from usuario";
+        $resultado = $pdo->query($sql);
+        //transformamos los registros en objetos:
+        $listado = array();
+        foreach ($resultado as $res) {
+            $usuario = new Usuario($res['idusuario'], $res['tipoidusuario'], $res['rolusuario'], $res['nombreusuario'],
+                    $res['fecnacusuario'],$res['ciunacusuario'], $res['direccionusuario'], $res['telefonousuario'], 
+                    $res['emailusuario'], $res['estadousuario']);
+            array_push($listado, $usuario);
+        }
+        Database::disconnect();
+        //retornamos el listado resultante:
+        return $listado;
+    }
+
+    /**
+     * Busca la informacion de un usuario en especifico.
+     * @param type $idusuario El numero de $idusuario del productos.
+     * @return type
+     */
+    public function getUsuario($idusuario) {
+        //obtenemos la informacion de la bdd:
+        $pdo = Database::connect();
+        $sql = "select * from usuario where idusuario=?";
+        $consulta = $pdo->prepare($sql);
+        $consulta->execute(array($idusuario));
+        //obtenemos el registro especifico:
+        $res = $consulta->fetch(PDO::FETCH_ASSOC);
+        $usuario = new Usuario($res['idusuario'], $res['tipoidusuario'], $res['rolusuario'], $res['nombreusuario'],
+                    $res['fecnacusuario'],$res['ciunacusuario'], $res['direccionusuario'], $res['telefonousuario'], 
+                    $res['emailusuario'], $res['estadousuario']);
+        Database::disconnect();
+        //retornamos el objeto encontrado:
+        return $usuario;
+    }
+
+    /**
+     * Inserta un nuevo usuario en la bdd.
+     * 
+     */
+    public function insertarUsuario($idusuario, $tipoidusuario, $rolusuario, $nombreusuario, $fecnacusuario, $ciunacusuario, $direccionusuario, $telefonousuario, $emailusuario, $estadousuario) {
+        $pdo = Database::connect();
+        $sql = "insert into usuario(idusuario, tipoidusuario, rolusuario, nombreusuario, fecnacusuario, ciunacusuario, direccionusuario, telefonousuario, emailusuario, estadousuario) values(?,?,?,?,?,?,?,?,?,?)";
+        $consulta = $pdo->prepare($sql);
+        //Ejecutamos y pasamos los parametros:
+        try {
+            $consulta->execute(array($idusuario, $tipoidusuario, $rolusuario, $nombreusuario, $fecnacusuario, $ciunacusuario, $direccionusuario, $telefonousuario, $emailusuario, $estadousuario));
+        } catch (PDOException $e) {
+            Database::disconnect();
+            throw new Exception($e->getMessage());
+        }
+        Database::disconnect();
+    }
+
+    /**
+     * Actualiza un usuario existente.
+     * 
+     */
+    public function actualizarUsuario($idusuario, $tipoidusuario, $rolusuario, $nombreusuario, $fecnacusuario, $ciunacusuario, $direccionusuario, $telefonousuario, $emailusuario, $estadousuario) {
+        //Preparamos la conexión a la bdd:
+        $pdo = Database::connect();
+        $sql = "update usuario set tipoidusuario=?,rolusuario=?,nombreusuario=?,fecnacusuario=?,ciunacusuario=?,direccionusuario=?,telefonousuario=?,emailusuario=?,estadousuario=? where idusuario=?";
+        $consulta = $pdo->prepare($sql);
+        //Ejecutamos la sentencia incluyendo a los parametros:
+        $consulta->execute(array($tipoidusuario, $rolusuario, $nombreusuario, $fecnacusuario, $ciunacusuario, $direccionusuario, $telefonousuario, $emailusuario, $estadousuario,$idusuario));
+        Database::disconnect();
+    }
+    
+    /**
+     * Elimina un usuario especifico de la bdd.
+     * @param type $idusuario
+     */
+    public function eliminarUsuario($idusuario){
+        //Preparamos la conexion a la bdd:
+        $pdo=Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql="delete from usuario where idusuario=?";
+        $consulta=$pdo->prepare($sql);
+        //Ejecutamos la sentencia incluyendo a los parametros:
+                try{
+            $consulta->execute(array($idusuario));
+        }  catch (PDOException $e){
+            Database::disconnect();
+            throw new Exception($e->getMessage());
+        }
+        Database::disconnect();
+    }
 }
