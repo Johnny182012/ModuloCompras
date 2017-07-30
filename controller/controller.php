d669d14432cf5ceb88ff157591b628fe053b2b53
Mayra <lorenaquelalq@gmail.com>
2017-07-30 12:09:57 -0500

    metodos proveedor controller

--- controller/controller.php
+++ controller/controller.php
@@ -106,4 +106,95 @@ switch ($opcion) {
         //si no existe la opcion recibida por el controlador, siempre
         //redirigimos la navegacion a la pagina index:
         header('Location: ../view/indexLogin.php');
+        
+        
+                //    -------------------LISTAR---------------------
+    // obtiene los datos de los proveedores de la base de datos
+    case "listar_proveedores":
+        //obtenemos la lista de empleados:
+        $listaProveedores = $crudModel->getProveedores();
+        //y los guardamos en sesion:
+        $_SESSION['listaProveedores'] = serialize($listaProveedores);
+        //redireccionamos a una nueva pagina para visualizar:
+        header('Location: ../view/proveedor.php');
+        break;
+
+    //crea un nuevo proveedor
+    case "crear_proveedor":
+        //obtenemos los parametros del formulario proveedor:
+        $idproveedor = $_REQUEST['idproveedor'];
+        $tipoidproveedor = $_REQUEST['tipoidproveedor'];
+        $nombreproveedor = $_REQUEST['nombreproveedor'];
+        $fecnacproveedor = $_REQUEST['fecnacproveedor'];
+        $ciudnacproveedor = $_REQUEST['ciudnacproveedor'];
+        $tipoproveedor = $_REQUEST['tipoproveedor'];
+        $direccionproveedor = $_REQUEST['direccionproveedor'];
+        $telefonoproveedor = $_REQUEST['telefonoproveedor'];
+        $emailproveedor = $_REQUEST['emailproveedor'];
+        $estadoproveedor = $_REQUEST['estadoproveedor'];
+        
+        //creamos el nuevo registro:
+        $crudModel->insertarProveedor($idproveedor, $tipoidproveedor, $nombreproveedor,$fecnacproveedor, $ciudnacproveedor, $tipoproveedor, $direccionproveedor, $telefonoproveedor,$emailproveedor,$estadoproveedor);
+        //actualizamos el listado:
+        $listaProveedores = $crudModel->getProveedores();
+        //y los guardamos en sesion:
+        $_SESSION['listaProveedores'] = serialize($listaProveedores);
+        //redireccionamos a una nueva pagina para visualizar:
+        header('Location: ../view/proveedor.php');
+        break;
+    
+     //elimina un empleado especifico
+    case "eliminar_proveedor":
+        //obtenemos el codigo del empleado a eliminar:
+        $idproveedor = $_REQUEST['idproveedor'];
+        //eliminamos del formulario:
+         try{
+            $crudModel->eliminarProveedor($idproveedor);
+        }catch(Exception $e){
+            //colocamos el mensaje de la excepcion en sesion:
+            $_SESSION['mensaje1']=$e->getMessage();
+        }
+        //actualizamos la lista de empleados para grabar en sesion:
+        $listaProveedores = $crudModel->getProveedores();
+        $_SESSION['listaProveedores'] = serialize($listaProveedores);
+        //redireccionamos a una nueva pagina para visualizar:
+        header('Location: ../view/proveedor.php');
+        break;
+    
+     //edita los datos de un empleado especifico
+    case "editar_proveedor":
+        //obtenemos los parametros del formulario:
+        $idproveedor = $_REQUEST['idproveedor'];
+        //Buscamos los datos
+        $listaProveedores = $crudModel->getProveedor($idproveedor);
+        //guardamos en sesion para edicion posterior:
+        $_SESSION['listaProveedores'] = serialize($listaProveedores);
+        //redirigimos la navegación al formulario de edicion profeedor:
+        header('Location: ../view/editarProveedor.php');
+        break;
+    
+    case "guardar_proveedor":
+        //obtenemos los parametros del formulario:
+        $idproveedor=$_REQUEST['idproveedor'];
+        if(isset($_SESSION['listaProveedores'])){
+            $listaProveedores=unserialize($_SESSION['listaProveedores']);
+            try {
+                $listaProveedores=$crudModel->guardarProveedor($listaProveedores, $idproveedor);
+                $_SESSION['listaProveedores']=$listaProveedores;
+                header('Location: ../view/proveedor_reporte.php');
+                break;
+            } catch (Exception $e) {
+                $mensajeError=$e->getMessage();
+                $_SESSION['mensajeError']=$mensajeError;
+            }
+        }
+        //actualizamos lista de facturas:
+        //$listado = $gastosModel->getFacturas();
+        //$_SESSION['listado'] = serialize($listado);
+        header('Location: ../view/proveedor.php');
+        break;
+    default:
+        //si no existe la opcion recibida por el controlador, siempre
+        //redirigimos la navegacion a la pagina index:
+        header('Location: ../view/index.php');
 }
